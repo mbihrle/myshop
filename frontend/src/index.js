adb7941243fb3b4482f45644720a5f1925382dc4
Marco Bihrle <bihrle.marco@hotmail.com>
2022-03-08 15:40:54 +0100

    create redux store, product list reducer & action

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    // <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>,
+    // </React.StrictMode>,
     document.getElementById('root')
 );
 
