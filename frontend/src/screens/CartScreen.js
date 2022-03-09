1f5c73ca63c22ddf5ec2bcf5901eaa430ffb7345
Marco Bihrle <bihrle.marco@hotmail.com>
2022-03-10 00:10:21 +0100

    add to cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+    Link,
+    useParams,
+    useSearchParams,
+    useNavigate,
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    Row,
+    Col,
+    LIstGroup,
+    Image,
+    Form,
+    Button,
+    Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+    const params = useParams();
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+
+    const productId = params.id;
+    const qty = searchParams.get('qty') ? Number(searchParams.get('qty')) : 1;
+
+    const dispatch = useDispatch();
+
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
     return <div>Cart</div>;
 };
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -19,7 +19,7 @@ import { listProductDetails } from '../actions/productActions';
 const ProductScreen = () => {
     const params = useParams();
     const navigate = useNavigate();
-    const [qty, setQty] = useState(0);
+    const [qty, setQty] = useState(1);
     const dispatch = useDispatch();
 
     const productDetails = useSelector((state) => state.productDetails);
