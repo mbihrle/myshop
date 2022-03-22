afaa90b522e8d32c33bba1ba0362ae17616506ae
Marco Bihrle <bihrle.marco@hotmail.com>
2022-03-22 03:29:58 +0100

    order screen

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -52,7 +52,7 @@ const orderSchema = mongoose.Schema(
             default: 0.0,
         },
         isPaid: {
-            type: String,
+            type: Boolean,
             required: true,
             default: false,
         },
